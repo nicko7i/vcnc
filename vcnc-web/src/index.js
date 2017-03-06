23685e97d16c4d2c02c2a910a780767178f6620c
Nick Okasinski <nicko7i@users.noreply.github.com>
2017-03-06 07:55:18 -0800

    vcnc-web: initialize the 'store' from reducers/initialState.js

--- vcnc-web/src/index.js
+++ vcnc-web/src/index.js
@@ -5,12 +5,13 @@ import {render} from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import initialState from './reducers/initialState';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import { syncHistoryWithStore } from 'react-router-redux';
 
-const store = configureStore();
+const store = configureStore(initialState);
 
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
