83105cc2f118daa75b7f6caf6d69fc0d68c6089b
Nick Okasinski <nicko7i@users.noreply.github.com>
2017-04-17 14:31:24 -0700

    vcnc web/server: push the mock dashboard data from vcnc-rest to vcnc-web over a websocket

--- vcnc-web/src/containers/ExternalEvents.jsx
+++ vcnc-web/src/containers/ExternalEvents.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Websocket from 'react-websocket';
 import * as actions from '../actions/realtimeActions';
