0e99d9579699d2569d7a6a61f98949c1bf2b26c6
Nick Okasinski <nicko7i@gmail.com>
2017-04-26 06:14:40 -0700

    vcnc-web: Factored a 'NumberWidget' out of 'StorageEfficiencyNumber'. Made the font size responsive to viewport width (but alas not the div width).

---
+++ vcnc-web/src/components/widgets/NumberWidget.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import muiThemeable from 'material-ui/styles/muiThemeable';
+import WidgetFrame from './WidgetFrame';
+
+const textStyle = {
+  position: 'relative',
+  float: 'left',
+  fontWeight: 'bold',
+  fontSize: '15vw',
+  top: '50%',
+  left: '50%',
+  transform: 'translateX(-50%) translateY(-50%)',
+};
+
+//
+// Text centering solution from http://stackoverflow.com/a/25799339/7702839
+const NumberWidget = props => (
+  <WidgetFrame title={props.title}>
+    <div style={{ width: '100%', height: props.muiTheme.dashboard.canvasHeight, position: 'relative' }}>
+      <div style={{ width: '100%', height: props.muiTheme.dashboard.canvasHeight, position: 'absolute' }}>
+        <div style={{ ...textStyle, fontSize: props.fontSize }} >
+          {props.value.toFixed(props.digits)}
+        </div>
+      </div>
+    </div>
+  </WidgetFrame>
+);
+
+
+NumberWidget.propTypes = {
+  canvasHeight: PropTypes.string,
+  digits: PropTypes.number,
+  fontSize: PropTypes.string,
+  muiTheme: PropTypes.object,
+  title: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+
+export default muiThemeable()(NumberWidget);
+
--- vcnc-web/src/containers/StorageEfficiencyNumber.jsx
+++ vcnc-web/src/containers/StorageEfficiencyNumber.jsx
@@ -2,36 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import muiThemeable from 'material-ui/styles/muiThemeable';
-import WidgetFrame from '../components/widgets/WidgetFrame';
+import NumberWidget from '../components/widgets/NumberWidget';
 
-const textStyle = {
-  position: 'relative',
-  float: 'left',
-  fontWeight: 'bold',
-  fontSize: '70pt',
-  top: '50%',
-  left: '50%',
-  transform: 'translateX(-50%) translateY(-50%)',
-};
-
-//
-// Text centering solution from http://stackoverflow.com/a/25799339/7702839
 const StorageEfficiencyNumber = props => (
-  <WidgetFrame title={props.title}>
-    <div style={{ width: '100%', height: props.muiTheme.dashboard.canvasHeight, position: 'relative' }}>
-      <div style={{ width: '100%', height: props.muiTheme.dashboard.canvasHeight, position: 'absolute' }}>
-        <div style={textStyle} >
-          {props.value.toFixed(1)}
-        </div>
-      </div>
-    </div>
-  </WidgetFrame>
+  <NumberWidget title={props.title} value={props.value} digits={1} fontSize="15vw" />
 );
 
 
 StorageEfficiencyNumber.propTypes = {
-  canvasHeight: PropTypes.string,
-  muiTheme: PropTypes.object,
   title: PropTypes.string.isRequired,
   value: PropTypes.number,
 };
