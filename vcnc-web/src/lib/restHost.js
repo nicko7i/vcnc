b92e2e2249865734a28273271d276e4f969ef29f
Nick Okasinski <nicko7i@users.noreply.github.com>
2017-04-28 09:10:50 -0700

    vcnc-web: optionally read the REST server host:port from the console URL.

--- vcnc-web/src/lib/restHost.js
+++ vcnc-web/src/lib/restHost.js
@@ -5,9 +5,25 @@
  */
 
 function restHost() {
+  //
+  //  Give the URL priority
+  //
+  const params = new URLSearchParams(window.location.search.substring(1));
+  const rest = params.get('rest');
+  if (rest) {
+    return rest;
+  }
+  //
+  //  Otherwise, if the URL is the Slingshot BrowserSync default, use the
+  //  pre-configured product default. This is where a developer's
+  //  vcnc-rest would be listening.
+  //
   if (window.location.host === 'localhost:3000') {
     return 'localhost:6130';
   }
+  //
+  //  Otherwise, use the offset convention programmed into vcnc-rest.
+  //
   const port = parseInt(window.location.port, 10) + 10;
   return `${window.location.hostname}:${port}`;
 }
