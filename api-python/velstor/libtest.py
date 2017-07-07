ef1205a469763cd96f023e80d3dcafc79bb65bc3
Nick Okasinski <nicko7i@users.noreply.github.com>
2017-07-07 14:56:36 -0700

    api-python: make the Mount class part of the high-level pcapi.

--- api-python/velstor/libtest.py
+++ api-python/velstor/libtest.py
@@ -1,11 +1,10 @@
-import string
-import random
-import subprocess
-import json
-import os
 import errno
-from velstor.pcapi.volume import Volume
-from velstor.pcapi.workspace import Workspace
+import json
+import string
+import subprocess
+
+import os
+import random
 
 
 class VclcError(Exception):
@@ -272,68 +271,3 @@ def random_path(length, depth, prefix="/"):
     return prefix + '/'.join(random_identifier(length) for i in range(depth))
 
 
-class Mount:
-    """
-    The unholy conjunction of a Volume and a Workspace.
-    
-    Reduces boilerplate when mounting VPs on short-lived workspaces.
-    
-    Args:
-        session: Security information.
-        mount_point: The local directory where the VP will mount.
-        **kwargs: Optional arguments passed to :class:`Workspace`.
-        
-    The constructor creates the specified workspace, puts it on the vtrq,
-    and mounts the VP.
-    
-    Calling dispose un-mounts the vp and removes the workspace
-    from the vtrq.
-    """
-    def __init__(self, session, mount_point, **kwargs):
-        self._workspace = Workspace(session, **kwargs)
-        self.volume = Volume(session, mount_point, self.workspace)
-        self.workspace.set(hard=True)
-        self.volume.mount(hard=True)
-
-    def dispose(self):
-        """
-        Unmounts the :class:`Volume` and deletes the :class:`Workspace`
-        on the vtrq.
-        """
-        self.volume.unmount()
-        self.workspace.delete(hard=True)
-
-    @property
-    def mount_point(self):
-        """str: Directory on which VP will be mounted."""
-        return self.volume.mount_point
-
-    @property
-    def workspace(self):
-        """Workspace: the Workspace component of this instance."""
-        return self._workspace
-
-    @property
-    def vtrq_id(self):
-        """int: Identifer of vtrq."""
-        return self.workspace.vtrq_id
-
-    @property
-    def vtrq_path(self):
-        """str: Absolute vtrq path mapped by this workspace."""
-        return self.workspace.vtrq_path
-
-    @property
-    def writeback(self):
-        """str:  Writeback value."""
-        return self.workspace.writeback
-
-    @property
-    def pathname(self):
-        """str:  Hierarchical workspace name."""
-        return self.workspace.pathname
-
-    @property
-    def is_private(self):
-        """bool: True if this workspace is 'private', False otherwise."""
-        return self.workspace.writeback != 'always'
